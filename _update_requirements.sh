c86af03c8385e9fa54286e2c5f418fbfe3f00270
Drew <21123428+drewski3420@users.noreply.github.com>
2026-03-09 10:55:04 -0400

    moved app files into app directory

--- _update_requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-pip install safety pip-tools
-rm -f requirements.txt
-pip-compile -r --output-file requirements.txt requirements.in 
-safety check -r requirements.txt
-
-pip install PdbBBEditSupport
