45b5a28d9f322b6aaae37117a8610946943d83b7
Drew <21123428+drewski3420@users.noreply.github.com>
2026-03-09 10:50:34 -0400

    removed server_settings.py, move these to env vars

---
+++ app/recast/settings.py
@@ -0,0 +1,174 @@
+# Django settings for feedthing project.
+
+import os
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+CLOUDFLARE_TOKEN = os.getenv("CLOUDFLARE_TOKEN","")
+CLOUDFLARE_ZONE = os.getenv("CLOUDFLARE_ZONE","")
+
+DEBUG = os.getenv("DEBUG", "False").lower() in ("1", "true", "yes")
+TEMPLATE_DEBUG = DEBUG
+
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS","").split(",")
+ADMINS = (
+    (os.getenv("ADMIN_NAME",""), os.getenv("ADMIN_EMAIL",""))
+)
+MANAGERS = ADMINS
+
+
+FEEDS_USER_AGENT = os.getenv("FEEDS_USER_AGENT","Recast/1.7")
+FEEDS_SERVER = os.getenv("FEEDS_SERVER","")
+FEEDS_CLOUDFLARE_WORKER = os.getenv("FEEDS_CLOUDFLARE_WORKER","")
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv("DATABASES_ENGINGE","django.db.backends.sqlite3"),
+         'NAME': os.getenv("DATABASES_FILENAME",os.path.join(SITE_ROOT, "db.sqlite3")),
+    }
+}
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS","").split(",")
+
+INTERNAL_IPS = ("127.0.0.1",)
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = "GMT"
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = "en-us"
+
+SITE_ID = 1
+
+USE_TZ = True
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = ""
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = ""
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.getenv("STATIC_ROOT", os.path.join(SITE_ROOT, "static"))
+
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = "/static/"
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+ADMIN_MEDIA_PREFIX = "/static/admin/"
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
+
+SECRET_KEY = os.getenv("SECRET_KEY","")
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
+)
+
+MIDDLEWARE = [
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "rc.middleware.PreviewMiddleware",
+    # 'smart_cache_control.middleware.SmartCacheControlMiddleware',
+]
+
+ROOT_URLCONF = "recast.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+INSTALLED_APPS = (
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # Uncomment the next line to enable the admin:
+    "django.contrib.admin",
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
+    "smart_cache_control",
+    "rc",
+    "feeds",
+)
+
+
+SCC_CUSTOM_URL_CACHE = ((r"/$", "private", 0),)
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "mail_admins": {"level": "ERROR", "class": "django.utils.log.AdminEmailHandler"}
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
